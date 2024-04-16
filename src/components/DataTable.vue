dedcc092096af8f52e98f73728d2ab4d6fe1ee5d
Brian Makumi <bmakumi2000@gmail.com>
2024-04-16 18:05:29 +0300

    first commit

---
+++ src/components/DataTable.vue
@@ -0,0 +1,60 @@
+<template>
+    <div v-if="posts">
+        <v-data-table 
+        v-model:items-per-page="itemsPerPage"
+        :headers="headers"
+        :items="data"
+        item-value='userId'
+        class='elevation-1'
+        >
+
+        </v-data-table>
+    </div>
+</template>
+
+<script>
+    import { VDataTable } from "vuetify/labs/components";
+    import getAllPost from "../services/getAllPost.js";
+
+    export default{
+        components: {
+            VDataTable
+        },
+        
+        setup() {
+            const { posts, error, load } = getAllPost()
+            load()
+            return { 
+                posts, 
+                error, 
+                load, 
+                itemsPerPage: 10, 
+                headers: [
+                    {
+                        title: 'User ID',
+                        align: 'start',
+                        sortable: false,
+                        key: 'userId'
+                    },
+                    {
+                        title: 'Title',
+                        align: 'start',
+                        key: 'title'
+                    },
+                    {
+                        title: 'Body',
+                        align: 'start',
+                        key: 'body'
+                    }
+                ],
+                data: posts
+
+            }
+        }
+    }
+
+</script>
+
+<style>
+    
+</style>
